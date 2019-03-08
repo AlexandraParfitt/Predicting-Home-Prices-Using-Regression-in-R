0856354c6073afe717e63dfdad7b2b931822c138
nmandarano <42852005+nmandarano@users.noreply.github.com>
2019-03-07 22:40:10 -0500

    Update and rename SemesterProject.Rmd to SemesterProject.r

--- SemesterProject.r
+++ SemesterProject.r
@@ -1,23 +1,15 @@
----
-title: "Semester Project"
-author: "Nicholas Mandarano"
-date: "3/5/2019"
-output: html_document
----
-
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
 library(readr)
 library(ggplot2)
 library(MASS)
+library(faraway)
 houses <- read.csv("houses.txt", header=T)
 attach(houses)
-```
 
-## Stepwise Regression
-
-```{r multiregression}
 lsr <- lm(price ~ bedrooms + bathrooms + sqft_living + sqft_lot + floors + sqft_above + sqft_basement + sqft_living15 + sqft_lot15 + grade + condition + yr_built + yr_renovated + zipcode + lat + long, data = houses)
+
+vif(lsr)
+
 bothstep.model <- stepAIC(lsr, direction = "both", trace = FALSE)
 summary(bothstep.model)
-```
+
+vif(bothstep.model)
